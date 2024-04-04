5c044ada734d0b648b3841a95b43681b41328434
abmackenthun <159589403+abmackenthun@users.noreply.github.com>
2024-04-03 21:47:53 -0500

    Add files via upload

---
+++ locallibrary/locallibrary/settings.py
@@ -0,0 +1,19 @@
+ INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # Add our new application
+    'catalog.apps.CatalogConfig', # This object was created for us in /catalog/apps.py
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+TIME_ZONE = 'Centeral'
